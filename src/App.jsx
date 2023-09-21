310a1f7d10b4e929a4ebc5a269206408fc116bae
orkeilius <orkeilius@users.noreply.github.com>
2023-09-21 10:43:39 +0200

    set up main page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    <div className='bg-gray-900 min-h-screen text-white font-sans'>
+      <div className='min-w-80 w-3/4 mx-auto'>
     <>
     </>
+      </div>
+    </div>
   )
 }
-
 export default App
