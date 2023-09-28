4ca0cc84ec8d4c2bdffebde40e585ba08f1f14b2
orkeilius <orkeilius@users.noreply.github.com>
2023-09-28 09:16:13 +0200

    add starfield

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,14 +6,14 @@ export default function Nav() {
                 <NavButton text="veille" link="/" />
                 <NavButton text="projet" link="/"/>
             </nav>
-            <div className="w-full h-[1px] bg-[linear-gradient(90deg,rgba(0,0,0,0)0%,rgba(255,255,255,1)1%,rgba(255,255,255,1)72%,rgba(255,255,255,1)99%,rgba(0,0,0,0)100%);]" />
+            <div className="mb-6 w-full h-[1px] bg-[linear-gradient(90deg,rgba(0,0,0,0)0%,rgba(255,255,255,1)1%,rgba(255,255,255,1)72%,rgba(255,255,255,1)99%,rgba(0,0,0,0)100%);]" />
         </>
     )
 }
 
 function NavButton({link ,text}){
     return (
-        <a href={link} className="hover:bg-slate-800 w-[4.25em] text-center p-2 mx-3 my-1 block rounded-3xl pt-2 ">
+        <a href={link} className="hover:border-b-4 w-[4.25em] transition-all text-center p-3 mx-3 my-1 block pt-1.5 mb-0 border-b-0">
             {text}
         </a>
     )
---
+++ src/components/Starfield.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Starfield() {
+
+    
+    const canvasRef = useRef(null)
+    
+    let mouseDown = false;
+    document.body.onmousedown = () => {
+        mouseDown = true;
+    };
+    document.body.onmouseup = () => {
+        mouseDown = false;
+    };
+
+    useEffect(() => {
+        const requestAnimFrame = (function () { return window.requestAnimationFrame })();
+        let centerX, centerY;
+        let numStars = 100;
+        let stars = [], star;
+
+
+        function executeFrame() {
+
+            requestAnimFrame(executeFrame);
+
+            if (canvas.width != window.innerWidth || canvas.height != window.innerHeight) {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+                centerX = canvas.width / 2;
+                centerY = canvas.height / 2;
+                initializeStars()
+            }
+            moveStars();
+            drawStars();
+        }
+
+        function initializeStars() {
+
+            stars = [];
+            for (let i = 0; i < numStars; i++) {
+                star = {
+                    x: ((Math.random()-0.5) * canvas.width) /4+ (canvas.width/2) ,
+                    y: ((Math.random()-0.5) * canvas.height ) /4+ (canvas.height/2),
+                    z: Math.random() * canvas.width,
+                    o: '0.' + Math.floor(Math.random() * 99) + 1
+                };
+                console.log(star.x)
+                stars.push(star);
+            }
+        }
+
+        function moveStars() {
+            for (let i = 0; i < numStars; i++) {
+                star = stars[i];
+                star.z -= mouseDown ? 4 : 2
+            }
+        }
+
+        function drawStars() {
+            var pixelX, pixelY, pixelRadius;
+            c.fillStyle = `rgba(0,10,10,${mouseDown ?0.005 :0.2 })`
+            c.fillRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < numStars; i++) {
+                star = stars[i];
+
+                pixelX = (star.x - centerX) * (focalLength / star.z);
+                pixelY = (star.y - centerY) * (focalLength / star.z);
+                pixelRadius = 1 * (focalLength / star.z) * 0.4;
+
+                if (
+                    Math.abs(pixelX) > canvas.width * 0.6 ||
+                    Math.abs(pixelY) > canvas.height * 0.6 ||
+                    star.z <= 0
+                ) {
+                    star.z = canvas.width
+                    star.x = ((Math.random()-0.5) * canvas.width) /4+ (canvas.width/2) 
+                    star.y = ((Math.random()-0.5) * canvas.height ) /4+ (canvas.height/2)
+                    continue
+                }
+                pixelY += centerY;
+                pixelX += centerX;
+
+                c.beginPath();
+                c.arc(pixelX, pixelY, pixelRadius, 0, 2 * Math.PI);
+                c.fillStyle = `rgba(255, 255, 255, ${1 / (star.z * 0.0015)})`;
+                c.fill();
+            }
+        }
+
+
+
+        const canvas = canvasRef.current
+        var c = canvas.getContext("2d");
+        const focalLength = canvas.width * 2;
+        initializeStars()
+        setInterval(executeFrame(), 1000 / 30);
+    }
+        , [])
+
+
+
+
+    return <canvas ref={canvasRef} className="w-full h-full"></canvas>
+}
